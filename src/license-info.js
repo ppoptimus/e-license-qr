015ba628eff043e26a192973bc4223c208673469
rpumpo <r.pumpo@gmail.com>
2023-03-06 09:10:47 +0700

    pat

--- src/license-info.js
+++ src/license-info.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function LicenseInfo() {
+  useEffect(() => {}, []);
+
+  const [licenseInfo, setLicenseInfo] = useState({
+    name: "สจจ.นนทบุรี",
+    address: "ศาลากลางจังหวัดนนทบุรี ชั้น 1 ถนนรัตนาธิเบศร์ บางกระสอ เมืองนนทบุรี นนทบุรี 11000",
+    telno: "0-2580-9276",
+    license_meneger: "นายกระปุกชัย ชนะศพ",
+    location: "https://goo.gl/maps/MZfed8FEGuYKRRfX9",
+  });
+  const getLicenseInfo = () => {
+    
+  };
   return (
-    <div>LicenseInfo</div>
-  )
+    <div className="container mt-4">
+      <h2>{licenseInfo.name}</h2>
+      <p>{licenseInfo.address}</p>
+      <p>โทรศัพท์ {licenseInfo.telno}</p>
+      <p>ผู้จัดการใบอนุญาต {licenseInfo.license_meneger}</p>
+      <p>
+        <span>ตำแหน่งที่ตั้ง </span>
+        <a href={licenseInfo.location}> คลิก</a>
+      </p>
+      <div className="row">
+        <img
+          src="https://img.freepik.com/free-vector/engraving-hand-drawn-ornamental-certificate_23-2148985846.jpg?t=st=1677644319~exp=1677644919~hmac=97a85cdfacaa2b1928f54af9f77c13c21e3a19c68e2aa72d23e583d418c3e0b9"
+          className="img img-fluid"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
--- src/receipt.js
+++ src/receipt.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Receipt() {
+  const [licenseInfo, setLicenseInfo] = useState({
+    request_name: "คำขอรับใบอนุญาตนำคนต่างด้าวเข้ามาทำงานกับนายจ้างในประเทศ",
+    company_name: "บริษัท ทีซีเอ็ม มหาชน จำกัด",
+    address: "ศาลากลางจังหวัดนนทบุรี ชั้น 1 ถนนรัตนาธิเบศร์ บางกระสอ เมืองนนทบุรี นนทบุรี 11000",
+    license_meneger: "นายกระปุกชัย ชนะศพ",
+    payment_date: "29/01/2556 14.30",
+  });
+  const getReceiptInfo = () => {
+    
+  };
   return (
-    <div>R</div>
-  )
+    <div className="container py-3">
+      <div className="card">
+        <div className="card-header">รายการขำขอ</div>
+        <div className="card-body">
+          <h5 className="card-title">{licenseInfo.request_name}</h5>
+          <p className="fw-bold">{licenseInfo.company_name}</p>
+
+          <span className="text-black-50">สถานที่ตั้ง</span>
+          <p className="fw-bold">{licenseInfo.address}</p>
+
+          <span className="text-black-50">ผู้จัดการหรือผู้แทนของนิติบุคคลผู้รับอนุญาต</span>
+          <p className="fw-bold">{licenseInfo.license_meneger}</p>
+          <p>ชำระค่าธรรมเนียมแล้ว วันที่ {licenseInfo.payment_date}</p>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        <img
+          src="https://img.freepik.com/free-vector/modern-flat-payment-receipt_23-2147911082.jpg?t=st=1677725917~exp=1677726517~hmac=b5359c78cbd582b91ba3c3c9b04dc61822bfac061f8c367d51e5a6b1eae73c89"
+          className="img img-fluid"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
